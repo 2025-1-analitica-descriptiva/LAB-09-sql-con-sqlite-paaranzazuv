e61065c0aff527d2ef41446bbb7c9facab88fdee
Pablo Andres Aranzazu V <paaranzazuv@unal.edu.co>
2025-06-21 11:01:23 -0500

    intento 2

--- homework/pregunta_13.sql
+++ homework/pregunta_13.sql
@@ -41,10 +41,10 @@
 --  3  D  709.530000
 --  4  E  474.825000
 -- 
---  >>> Escriba su codigo a partir de este punto <<<
+--  >>> Escribag su codigo a partir de este punto <<<
 --  
 SELECT 
-  K0, 
+  K0,
   ROUND(AVG(c12), 2) AS "avg(c12)"
 FROM tbl1
 WHERE c13 > 400
